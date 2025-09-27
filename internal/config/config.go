45f988f9680f49049669a7c4709316f07b2aca79
Gérald <gerald.leban0@gmail.com>
2025-09-27 15:20:38 +0200

    feat: add redis env variables

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,17 +9,31 @@ import (
 
 type Config struct {
 	MusicLibraryPath string
+	RedisAddr        string
+	RedisPassword    string
+}
+
+const (
+	MUSIC_LIBRARY_PATH = "MUSIC_LIBRARY_PATH"
+	REDIS_ADDR         = "REDIS_ADDR"
+)
+
+func getEnv(name string) string {
+	variable := os.Getenv(name)
+	if variable == "" {
+		log.Fatalf("%s environment variable not set", name)
+	}
+	return variable
 }
 
 func LoadConfig() *Config {
 	_ = godotenv.Load()
 
-	musicPath := os.Getenv("MUSIC_LIBRARY_PATH")
-	if musicPath == "" {
-		log.Fatalf("MUSIC_LIBRARY_PATH environment variable not set")
-	}
+	musicPath := getEnv(MUSIC_LIBRARY_PATH)
+	redisAddr := getEnv(REDIS_ADDR)
 
 	return &Config{
 		MusicLibraryPath: musicPath,
+		RedisAddr:        redisAddr,
 	}
 }
