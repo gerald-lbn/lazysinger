286ab5ce040d6f05dd5eb94210395ade01b0f4db
Gérald <gerald.leban0@gmail.com>
2025-10-05 13:02:50 +0200

    feat: add scheduler and tests

---
+++ internal/scheduler/scheduler.go
@@ -0,0 +1,45 @@
+package scheduler
+
+import (
+	"context"
+
+	"github.com/gerald-lbn/lazysinger/internal/singleton"
+	"github.com/robfig/cron/v3"
+)
+
+type Scheduler interface {
+	Run(ctx context.Context)
+	Add(crontab string, cmd func()) (int, error)
+	Remove(id int)
+}
+
+type scheduler struct {
+	c *cron.Cron
+}
+
+func (s *scheduler) Run(ctx context.Context) {
+	s.c.Start()
+	<-ctx.Done()
+	s.c.Stop()
+}
+
+func (s *scheduler) Add(crontab string, cmd func()) (int, error) {
+	entryID, err := s.c.AddFunc(crontab, cmd)
+	if err != nil {
+		return 0, err
+	}
+	return int(entryID), nil
+}
+
+func (s *scheduler) Remove(id int) {
+	s.c.Remove(cron.EntryID(id))
+}
+
+func GetInstance() Scheduler {
+	return singleton.GetInstance(func() *scheduler {
+		c := cron.New()
+		return &scheduler{
+			c: c,
+		}
+	})
+}
