d903d40bd4b9e758603b0304a1f1ab315ef985af
Gérald Leban <gerald.leban0@gmail.com>
2025-10-18 18:50:32 +0200

    feat: push task with filepath in queue

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -2,21 +2,50 @@ package scanner
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"io/fs"
 	"path/filepath"
 
 	"github.com/gerald-lbn/lazysinger/config"
 	"github.com/gerald-lbn/lazysinger/log"
+	"github.com/gerald-lbn/lazysinger/music"
+	"github.com/gerald-lbn/lazysinger/worker"
+	"github.com/hibiken/asynq"
 )
 
 func ScanAll(ctx context.Context) error {
+	asynqClient := asynq.NewClient(asynq.RedisClientOpt{
+		Addr: config.Server.Worker.RedisAddr,
+	})
+
 	return filepath.WalkDir(config.Server.Scanner.Directory, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			log.Error().Err(err).Str("path", path).Msg("Unable to walk down the file tree")
 		}
 
-		fmt.Println(path)
+		if !d.IsDir() && music.IsMusicFile(path) {
+			task, err := worker.NewDownloadLyricsTaskHandler(path)
+			if err != nil {
+				log.Error().Err(err).Str("path", path).Msg("Unable to create download task")
+				return err
+			} else {
+				log.Debug().Str("path", path).Msg("Download task created successfully")
+			}
+
+			taskInfo, err := asynqClient.Enqueue(task, asynq.TaskID(path))
+			switch {
+			case errors.Is(err, asynq.ErrDuplicateTask):
+				{
+					log.Warn().Err(err).Msg("This track already has a task in queue, skipping...")
+				}
+			case err != nil:
+				{
+					log.Error().Err(err).Str("path", path).Msg("Unable to enqueue download task")
+					return err
+				}
+			}
+			log.Info().Str("path", path).Str("task_id", taskInfo.ID).Msg("Task pushed to queue")
+		}
 
 		return nil
 	})
