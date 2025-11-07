bb5bcb6b7f8d4e008a5bed9600353ee7ed813a2f
Gérald Leban <gerald.leban0@gmail.com>
2025-11-07 16:08:52 +0100

    docs: add comments for types, fields, functions and methods

--- music/metadata.go
+++ music/metadata.go
@@ -1,17 +1,26 @@
 package music
 
-import "go.senan.xyz/taglib"
+import (
+	"go.senan.xyz/taglib"
+)
 
+// Metadata contains the metadata properties of an audio file
 type Metadata struct {
-	Title    *string
-	Artist   *string
-	Album    *string
+	// Title is the name of the audio
+	Title *string
+	// Artist is the album artist's name of the audio
+	Artist *string
+	// Album is the name of the album the audio belongs to
+	Album *string
+	// Duration is the length of the audio in seconds
 	Duration float64
 }
 
 func (m *Metadata) HasAllMetadata() bool {
 	return m.Title != nil && m.Artist != nil && m.Album != nil
 }
+
+// ExtractMetadata extracts metadata from an audio file specified by its path.
 func ExtractMetadata(p string) (*Metadata, error) {
 	tags, err := taglib.ReadTags(p)
 	if err != nil {
