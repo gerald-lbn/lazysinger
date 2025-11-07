f0cd449149474fd7d31ae21012438ea5b5574ea7
Gérald Leban <gerald.leban0@gmail.com>
2025-11-07 16:33:41 +0100

    feat: concurrently run the watcher

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/gerald-lbn/refrain/watcher"
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	ctx, cancel := mainContext(context.Background())
+	defer cancel()
+
+	runRefrain(ctx)
+}
+
+// mainContext returns a context that is cancelled when the process receives a signal to exit.
+func mainContext(ctx context.Context) (context.Context, context.CancelFunc) {
+	return signal.NotifyContext(
+		ctx,
+		os.Interrupt,
+		syscall.SIGHUP,
+		syscall.SIGTERM,
+		syscall.SIGABRT,
+	)
+}
+
+func runRefrain(ctx context.Context) {
+	log.Print("Starting Refrain")
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	g.Go(startWatcher(ctx))
+
+	if err := g.Wait(); err != nil {
+		log.Print("A fatal error occured in Refrain. Aborting", err)
+	}
+}
+
+func startWatcher(ctx context.Context) func() error {
+	log.Print("Starting watcher")
+
+	return func() error {
+		w, err := watcher.NewWatcher()
+		if err != nil {
+			return err
+		}
+
+		path := "/music"
+		if err := w.Start(ctx, path); err != nil {
+			log.Fatalf("An error occured while watching: '%s'. Reason: %v", path, err)
+		}
+		defer w.Stop()
+
+		return nil
+	}
+}
