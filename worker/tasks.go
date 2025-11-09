ca510e6497828b6fe93ccdcb0cd651f7b4e8e460
Gérald Leban <gerald.leban0@gmail.com>
2025-11-09 20:32:28 +0100

    feat: write download fetched lyrics content

--- worker/tasks.go
+++ worker/tasks.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gerald-lbn/refrain/music"
 	"github.com/gerald-lbn/refrain/music/lrclib"
@@ -34,31 +35,55 @@ func NewDownloadLyricsTask(metadata music.Metadata) (*asynq.Task, error) {
 }
 
 func HandleDownloadLyricsTask(ctx context.Context, t *asynq.Task) error {
-	var p DownloadLyricsPayload
-	if err := json.Unmarshal(t.Payload(), &p); err != nil {
+	var track DownloadLyricsPayload
+	if err := json.Unmarshal(t.Payload(), &track); err != nil {
 		return fmt.Errorf("json.Unmarshal failed: %v: %w", err, asynq.SkipRetry)
 	}
 
 	// Skip task if track already has both lyrics
-	if p.HasBothLyricsStoredLocally() {
+	if track.HasBothLyricsStoredLocally() {
 		return asynq.RevokeTask
 	}
 
 	provider := lrclib.NewLRCLibProvider()
 	var options lrclib.SearchLyricsOptions
-	if p.HasAllMetadata() {
-		options = lrclib.WithTrackArtistAndAlbumName(*p.Title, *p.Artist, *p.Album)
-	} else if p.Artist == nil || p.Title == nil {
-		log.Printf("skipping track: '%s'. Reason: Not enough metadata to search", p.Path)
+	if track.HasAllMetadata() {
+		options = lrclib.WithTrackArtistAndAlbumName(*track.Title, *track.Artist, *track.Album)
+	} else if track.Artist == nil || track.Title == nil {
+		log.Printf("skipping track: '%s'. Reason: Not enough metadata to search", track.Path)
 		return asynq.SkipRetry
 	} else {
-		options = lrclib.WithTrackAndArtistName(*p.Title, *p.Artist)
+		options = lrclib.WithTrackAndArtistName(*track.Title, *track.Artist)
 	}
 
-	_, err := provider.GetLyrics(ctx, options, int(p.Duration))
+	lyrics, err := provider.GetLyrics(ctx, options, int(track.Duration))
 	if err != nil {
-		log.Printf("skipping track: '%s'. Reason: %s", p.Path, err)
+		log.Printf("skipping track: '%s'. Reason: %s", track.Path, err)
 		return asynq.SkipRetry
+	}
+
+	if lyrics == nil {
+		log.Printf("skipping track: '%s'. Empty lyrics", track.Path)
+		return nil
+	}
+
+	// Skip instrumental track
+	if lyrics.Instrumental {
+		return nil
+	}
+
+	if len(lyrics.PlainLyrics) > 0 && !track.HasPlainLyrics {
+		err = os.WriteFile(track.PlainLyricsPath, []byte(lyrics.PlainLyrics), 0644)
+		if err != nil {
+			log.Printf("Failed to write to '%s'. Reason: %s", track.PlainLyricsPath, err)
+		}
+	}
+
+	if len(lyrics.SyncedLyrics) > 0 && !track.HasSyncedLyrics {
+		err = os.WriteFile(track.SyncedLyricsPath, []byte(lyrics.SyncedLyrics), 0644)
+		if err != nil {
+			log.Printf("Failed to write to '%s'. Reason: %s", track.SyncedLyricsPath, err)
+		}
 	}
 
 	return nil
