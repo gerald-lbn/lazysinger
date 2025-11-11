a7bc1c2b01637589728cf8108f493988fd384ff3
Gérald Leban <gerald.leban0@gmail.com>
2025-11-11 12:06:58 +0100

    feat: create en enqueue task when a music is detected

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/gerald-lbn/refrain/pkg/services"
+	"github.com/gerald-lbn/refrain/pkg/tasks"
+	"github.com/gerald-lbn/refrain/pkg/utils/file"
 )
 
 func main() {
@@ -26,6 +28,31 @@ func main() {
 		slog.Info("create event detected",
 			"operation", event.Op.String(),
 			"path", event.Name)
+
+		ok, err := file.IsAudioFile(event.Name)
+		if err != nil {
+			return err
+		}
+
+		if !ok {
+			return nil
+		}
+
+		task, err := tasks.NewDownloadLyricsTask(event.Name)
+		if err != nil {
+			return err
+		}
+
+		info, err := c.Worker.EnqueueTask(task)
+		if err != nil {
+			return err
+		}
+
+		slog.Info("enqueued download lyrics task",
+			"task_id", info.ID,
+			"file_path", event.Name,
+		)
+
 		return nil
 	})
 
@@ -50,6 +77,8 @@ func main() {
 			"path", event.Name)
 		return nil
 	})
+
+	c.Worker.RegisterHandler(tasks.TypeDownloadLyrics, tasks.HandleDownloadLyricsTask)
 
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
