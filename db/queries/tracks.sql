6efc8cab907bb22052e273e99c78aca06f295e3b
Gérald Leban <gerald.leban0@gmail.com>
2025-11-16 09:04:54 +0100

    feat: add queries

---
+++ db/queries/tracks.sql
@@ -0,0 +1,41 @@
+-- name: GetAllTracks :many
+SELECT * FROM tracks;
+
+-- name: GetTrackByPath :one
+SELECT * FROM tracks WHERE path = ? LIMIT 1;
+
+-- name: CreateTrack :exec
+INSERT INTO tracks (
+    path,
+    title,
+    artist,
+    album,
+    duration,
+    has_plain_lyrics,
+    has_synced_lyrics
+) VALUES (?, ?, ?, ?, ?, ?, ?);
+
+-- name: UpdateTrack :exec
+UPDATE tracks
+SET
+    title = ?,
+    artist = ?,
+    album = ?,
+    duration = ?,
+    has_plain_lyrics = ?,
+    has_synced_lyrics = ?
+WHERE path = ?;
+
+-- name: DeleteTrack :exec
+DELETE FROM tracks WHERE path = ?;
+
+-- name: GetTracksByArtist :many
+SELECT * FROM tracks WHERE artist = ? ORDER BY album, title;
+
+-- name: GetTracksByAlbum :many
+SELECT * FROM tracks WHERE album = ? ORDER BY title;
+
+-- name: SearchTracks :many
+SELECT * FROM tracks
+WHERE title LIKE ? OR artist LIKE ? OR album LIKE ?
+ORDER BY artist, album, title;
